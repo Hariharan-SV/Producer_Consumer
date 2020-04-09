12d98810a35c0f68fbf19af1c1ffeb2e174f9249
Hariharan20 <harikumar20112000@gmail.com>
2020-04-09 19:20:59 +0530

    main file asks runtime,devices,queuelen as cmd arg

--- main.c
+++ main.c
@@ -6,17 +6,17 @@
 #include <sys/ipc.h>
 #include <sys/types.h>
 
-#define MAX_SIZE 4
 
 struct Memory{
-	int status;
-	int data[4];
+	int no_of_devices;
+	int queue_size;
 };
+
 
 int main(int argc,char *argv[]) {
 
-	if(argc!=2){
-		printf("\nInsufficient arguments give ./a.out <total_run_time>");
+	if(argc!=7){
+		printf("\nInsufficient arguments give ./a.out -t <total_run_time> -d <no_of_printers> -l <length_of_queue>");
 		exit(1);
 	}
 
@@ -26,41 +26,41 @@ int main(int argc,char *argv[]) {
 	int	ShmID;
 	struct Memory *ShmPTR;
 	int runtime;
+		
+	ShmKEY = 2011;
 
-	runtime=atoi(argv[1]);
-
-	ShmKEY=2011;
-
-	ShmID=shmget(ShmKEY,sizeof(struct Memory),IPC_CREAT| 0666);
+	ShmID = shmget(ShmKEY,sizeof(struct Memory),IPC_CREAT| 0666);
 	if(ShmID<0){
 		printf("\n shmget() Failed");
 		exit(1);
 	}
 
-	ShmPTR=(struct Memory *)shmat(ShmID,NULL,0);
+	ShmPTR = (struct Memory *)shmat(ShmID,NULL,0);
 	if((int)ShmPTR == -1){
 		printf("\n shmat() Failed");
 		exit(1);
 	}
 
-	printf("Shared Memory Centre SHMID is %d \n",ShmID,ShmID);
+	printf("Photocopy Centre's SHMID is %d \n",ShmID);
+	ShmPTR->no_of_devices = atoi(argv[4]);
+	ShmPTR->queue_size = atoi(argv[6]);
+	printf("no_of_devices:%d\nqueue_size:%d\n",ShmPTR->no_of_devices,ShmPTR->queue_size);
 
-
+	runtime = atoi(argv[2]);
 	start = time(NULL);
 	int terminate = 1;
 	while (terminate) {
 		end = time(NULL);
 		elapsed = difftime(end, start);
+
 		if (elapsed >= (double)runtime)
 			terminate = 0;
 		else
 			sleep(1);
 	}
-
-
 	shmdt((void*)ShmPTR);
-	shmctl(ShmID,IPC_RMID,NULL);
-
+	shmctl(ShmID,IPC_RMID,NULL);	
 	printf("Shutting the centre without abnormal termination..\n");
+
 	return 0;
  }
