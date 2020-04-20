56d440c78b4612af5b6b9b755576c8a42e26565b
Deepan <50180664+DeepanNarayanaMoorthy@users.noreply.github.com>
2020-04-20 19:19:00 +0530

    Documentation made for circular_queue.h

--- circular_queue.h
+++ circular_queue.h
@@ -1,11 +1,11 @@
 #include "photocopy.h"
 
 struct Circular_Queue{
-    int MAX;
-    int front;
-    int rear;
-    int no_of_devices;
-    int status;
-    int lock;
-    struct Photocopy *cqueue_arr;
+    int MAX;                            // A Macro assigned for Maximum number of elements in a Circular Queue
+    int front;                          // Pointer pointing the front of the Queue
+    int rear;                           // Pointer pointing the rear of the Queue
+    int no_of_devices;                  // Number of Devices in Circular Queue
+    int status;                         // Status of each device in Circular Queue
+    int lock;                           // Lock value
+    struct Photocopy *cqueue_arr;       // Photocopy Structure to be assigned as an element in the Circular Queue
 };
