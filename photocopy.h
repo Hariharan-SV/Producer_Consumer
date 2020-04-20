f9a1c4c1a1a2987c1fc845362fe0ffa044925c24
Deepan <50180664+DeepanNarayanaMoorthy@users.noreply.github.com>
2020-04-20 19:15:21 +0530

    Documentation for photocopy.h made

--- photocopy.h
+++ photocopy.h
@@ -1,5 +1,5 @@
 struct Photocopy{
-	char filename[20];
-	int filesize;
-	int copies;
+	char filename[20]; // Name of the file
+	int filesize;      // Size of the File
+	int copies;        // Number of Copies to be made 
 };
